de3228d9ebfebd4e2e298b05a09a889c9b785e9c
Hugo Dolan <hugojdolan@gmail.com>
2020-07-04 15:06:27 +0100

    =database implemented

--- web.py
+++ web.py
@@ -2,6 +2,87 @@ import websockets
 import json
 import asyncio
 import time
+import sqlite3
+from sqlite3 import Error
+
+class Database:
+    def __init__(self):
+        self._db_path = './datasets/markets.db'
+        self._setup_path = './datasets/markets_setup.txt'
+        self.verbose = True
+
+    def log(self, s):
+        if self.verbose:
+            print(s)
+
+    def create_connection(self, db_file):
+        """ create a database connection to the SQLite database specified by db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        self.conn = None
+        try:
+            self.conn = sqlite3.connect(db_file)
+        except Error as e:
+            print('[Database] Error:')
+            print(e)
+        
+        return self.conn
+
+    def run_setup_script(self, script_path):
+        """ create a table from the create_table_sql statement
+        :param conn: Connection object
+        :param create_table_sql: a CREATE TABLE statement
+        :return:
+        """
+        try:
+            f = open(script_path, 'r')
+            setup_script = f.read()
+            print(setup_script)
+            c = self.conn.cursor()
+            c.executescript(setup_script)
+        except (Error, IOError) as e:
+            print('[Datanase] Error:')
+            print(e)    
+
+    def __enter__(self):
+        if self.create_connection(self._db_path):
+            self.run_setup_script(self._setup_path)
+            self.log('[Database] Connected to db @ %s' % self._db_path)
+            return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            # Always close db when finished
+            self.log('[Database] Disconnected from db @ %s' % self._db_path)
+            self.conn.commit()
+            self.conn.close()
+
+    def update_prices(self, books):
+        try:
+            c = self.conn.cursor()
+            
+            for ticker, book in books.items():
+                timestamp = time.time()
+                best_bid = book['best_bid']
+                best_ask = book['best_ask']
+                bid_depth = book['bid_depth']
+                ask_depth = book['ask_depth']
+                bid_volume = book['bid_volume']
+                ask_volume = book['ask_volume']
+                n_bids = book['n_bids']
+                n_asks = book['n_asks']
+
+                sql = """INSERT OR REPLACE INTO prices (timestamp,ticker,best_bid,best_ask, bid_depth, ask_depth, bid_volume, ask_volume, n_bids, n_asks) VALUES (?,?,?,?,?,?,?,?,?,?)"""
+
+                c.execute(sql, (timestamp, ticker, best_bid, best_ask, bid_depth, ask_depth, bid_volume, ask_volume, n_bids, n_asks))
+
+                print((timestamp, ticker, best_bid, best_ask, bid_depth, ask_depth, bid_volume, ask_volume, n_bids, n_asks))
+
+            self.conn.commit()
+        except Error as e:
+            print('[Database] Error:')
+            print(e)
 
 class WebServer:
     def __init__(self, backend_config_dir = './configs/backend_config.json', frontend_config_dir = './configs/frontend_config.json', ):
@@ -60,33 +141,39 @@ class WebServer:
             if is_backend:
                 print("Backend connection established")
                 # Store the data for sending to the server.
-                while True:
-                    data = await ws.recv()
-                    msg = json.loads(data)
-                    msg_type = msg['type']
 
-                    # This must always be the first message sent from the server
-                    if msg_type == 'config':
-                        self.backend_to_app_cache.append(data)
-                        self.backend_to_app_config_cache.append(data)
-                        self.n_config_messages = msg['n_config_messages']
-                    # Handles caching of key setup information
-                    elif len(self.backend_to_app_config_cache) < self.n_config_messages:
-                        self.backend_to_app_config_cache.append(data)
+                # Create SQL Connection
+                with Database() as db:
+                    while True:
+                        data = await ws.recv()
+                        msg = json.loads(data)
+                        msg_type = msg['type']
+
+                        if msg['type'] == 'LOBS':
+                            db.update_prices(msg['data'])
+
+                        # This must always be the first message sent from the server
+                        if msg_type == 'config':
+                            self.backend_to_app_cache.append(data)
+                            self.backend_to_app_config_cache.append(data)
+                            self.n_config_messages = msg['n_config_messages']
+                        # Handles caching of key setup information
+                        elif len(self.backend_to_app_config_cache) < self.n_config_messages:
+                            self.backend_to_app_config_cache.append(data)
 
 
-                    if msg_type in self.last_posts:
-                        if self.last_posts[msg_type] < time.time() - self._debounce:
+                        if msg_type in self.last_posts:
+                            if self.last_posts[msg_type] < time.time() - self._debounce:
+                                self.backend_to_app_cache.append(data)
+                                self.backend_has_updates.set()
+                                self.last_posts[msg_type] = time.time() 
+                        else:
                             self.backend_to_app_cache.append(data)
                             self.backend_has_updates.set()
-                            self.last_posts[msg_type] = time.time() 
-                    else:
-                        self.backend_to_app_cache.append(data)
-                        self.backend_has_updates.set()
             
             elif is_app: 
                 print("App [Backend State Management] connection established")
-
+            
                 # Send any critical setup information once off
                 for data in self.backend_to_app_config_cache:
                     await ws.send(data)
@@ -140,3 +227,5 @@ class WebServer:
 
 """Start the webserver"""
 WebServer()
+
+
