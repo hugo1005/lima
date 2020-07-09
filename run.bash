f1b23d6e117094cc1eb27cdbb005ce7ba69f2fe5
Hugo Dolan <hugojdolan@gmail.com>
2020-07-09 10:01:00 +0100

    =rebooting bash script added

--- run.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-osascript -e 'tell app "Terminal"
-	do script "cd documents/luno/lima; python3 ./web.py"
-end tell'
-
-osascript -e 'tell app "Terminal"
-	do script "cd documents/luno/lima; python3 ./backend.py"
-end tell'
